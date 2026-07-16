7df33e99d4c79d2699b531520ddc2d096a2fbabe
IfeomaEzeka <ifeomaezeka01@gmail.com>
2026-07-16 05:53:35 +0100

    Update to query

--- Lessons/1.20/Data_types.sql
+++ Lessons/1.20/Data_types.sql
@@ -1,4 +1,9 @@
 SELECT 
-    *
+    table_name,
+    column_name,
+    data_type
+
 FROM 
-    information_schema.columns;
+    information_schema.columns
+WHERE
+    table_name = 'job_postings_fact';
