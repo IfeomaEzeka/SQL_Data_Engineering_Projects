63757be216fa9aef0a2dab79d9cd0c066247ef41
IfeomaEzeka <ifeomaezeka01@gmail.com>
2026-08-12 20:59:41 +0100

    Carried out more DML operations

--- Lessons/1.21/DML.sql
+++ Lessons/1.21/DML.sql
@@ -13,5 +13,21 @@ ALTER TABLE staging.preferred_roles
 ADD COLUMN preferred_role BOOLEAN ;
 
 UPDATE staging.preferred_roles
-SET preferred_role = TRUE;
+SET preferred_role = TRUE
 WHERE role_id BETWEEN 1 AND 4; -- TO BE USED ONLY WHEN YOU HAVE SPECIFIC ROWS . IF THE CHANGE IS FOR ALL THE ROWS, IT IS NOT NEEDED
+
+ALTER TABLE staging.preferred_roles
+RENAME TO priority_roles;
+
+SELECT * 
+FROM staging.priority_roles;
+
+ALTER TABLE staging.priority_roles
+RENAME COLUMN preferred_role TO priority_lvl;
+
+ALTER TABLE staging.priority_roles
+ALTER COLUMN priority_lvl TYPE INT;
+
+UPDATE staging.priority_roles
+SET priority_lvl = 2
+WHERE role_id = 2;
