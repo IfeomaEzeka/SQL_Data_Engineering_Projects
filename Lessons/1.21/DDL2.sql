0c514eb814196af5a5124d46bd3217ea2adbea14
IfeomaEzeka <ifeomaezeka01@gmail.com>
2026-09-21 19:22:11 +0100

    Added select statements for debugging

--- Lessons/1.21/DDL2.sql
+++ Lessons/1.21/DDL2.sql
@@ -60,3 +60,42 @@ SELECT
     COUNT(*) AS job_count
 FROM  hourly_jobs_flat_temp
 GROUP BY job_title_short;
+
+SELECT COUNT(*) FROM  hourly_jobs_flat_temp;
+SELECT COUNT(*) FROM  staging.priority_jobs_flat_view;
+SELECT COUNT(*) FROM  staging.job_postings_flat;
+
+--Removing all jobs posted before 2024
+DELETE FROM staging.job_postings_flat
+WHERE job_posted_date < '2024-01-01';
+
+SELECT COUNT(*) FROM  hourly_jobs_flat_temp;
+SELECT COUNT(*) FROM  staging.priority_jobs_flat_view;
+SELECT COUNT(*) FROM  staging.job_postings_flat;
+
+TRUNCATE TABLE staging.job_postings_flat;
+
+SELECT * FROM staging.job_postings_flat;
+
+INSERT INTO staging.job_postings_flat
+SELECT
+    jpf.job_id,
+    jpf.job_title_short,
+    jpf.job_title,
+    jpf.job_location,
+    jpf.job_via,
+    jpf.job_schedule_type,
+    jpf.job_work_from_home,
+    jpf.search_location,
+    jpf.job_posted_date,
+    jpf.job_no_degree_mention,
+    jpf.job_health_insurance,
+    jpf.job_country,
+    jpf.salary_rate,
+    jpf.salary_year_avg,
+    jpf.salary_hour_avg,
+    cd.name AS company_name
+FROM data_jobs.job_postings_fact AS jpf
+LEFT JOIN data_jobs.company_dim AS cd
+    ON jpf.company_id = cd.company_id
+WHERE job_posted_date >= '2024-01-01';
